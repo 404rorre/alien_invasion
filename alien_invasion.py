5b9648ec77f5b51ca2ad67c2f05e36572630c82c
404rorre <404rorre.edoc@gmail.com>
2022-05-11 15:09:06 +0200

    Create movement to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,8 @@ class AlienInvasion:
 		self.settings = Settings()
 		self.screen = pygame.display.set_mode((
 					self.settings.screen_width, 
-					self.settings.screen_height))
+					self.settings.screen_height)
+					)
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
 
@@ -28,6 +29,11 @@ class AlienInvasion:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					#Move the ship to the right.
+					self.ship.rect.x += 1
+					
 	def _update_screen(self):
 		"""Update images on the screen and flip to the new screen."""
 		self.screen.fill(self.settings.bg_color)
