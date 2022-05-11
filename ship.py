0267c90d2eb78c75423ae395fdbfb38f0c15f1ae
404rorre <404rorre.edoc@gmail.com>
2022-05-11 15:17:09 +0200

    Ship: continuous movement to the right

--- ship.py
+++ ship.py
@@ -11,6 +11,13 @@ class Ship:
 		self.rect = self.image.get_rect() #uses rectangles values of the ship
 		#Start each new ship at the bottom center of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
+		#Movement flag
+		self.moving_right = False
+
+	def update(self):
+		"""Update the ship's position bases on the movement flag."""
+		if self.moving_right:
+			self.rect.x += 1
 
 	def blitme(self):
 		"""Draw the ship at its current location."""
