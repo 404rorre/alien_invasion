2f61eb74914f3077c166bda77db7377c5cbee6a9
404rorre <404rorre.edoc@gmail.com>
2022-05-12 13:52:06 +0200

    Created Full Screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,10 +10,11 @@ class AlienInvasion:
 		"""Initialize the game, an create game ressources."""
 		pygame.init()
 		self.settings = Settings()
-		self.screen = pygame.display.set_mode((
-					self.settings.screen_width, 
-					self.settings.screen_height
-					))
+		self.screen = pygame.display.set_mode(
+					(0,0),pygame.FULLSCREEN
+					)
+		self.settings.screen_width = self.screen.get_rect().width
+		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
 		#system flags
