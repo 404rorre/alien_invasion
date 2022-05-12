87f76754fb26e4c3cf163e3d84c44aa28e1ef1ca
404rorre <404rorre.edoc@gmail.com>
2022-05-12 12:39:29 +0200

    createed movement limit to ship according to screen border

--- ship.py
+++ ship.py
@@ -20,10 +20,12 @@ class Ship:
 
 	def update(self):
 		"""Update the ship's position bases on the movement flag."""
-		if self.moving_right:
-			self.x += self.settings.ship_speed
-		if self.moving_left:
-			self.x -= self.settings.ship_speed
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			if self.moving_right:
+				self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			if self.moving_left:
+				self.x -= self.settings.ship_speed
 
 		self.rect.x = self.x
 
