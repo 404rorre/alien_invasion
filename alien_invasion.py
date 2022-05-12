336f6454607125a86d2fb85ecb4e97acbd0e471b
404rorre <404rorre.edoc@gmail.com>
2022-05-12 13:32:11 +0200

    refactored _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,22 +28,10 @@ class AlienInvasion:
 		"""Respond to keypress and mousevents."""
 		#Watch for keyboard and mouse events.
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					#Move the ship to the right.
-					self.ship.moving_right = True
-				if event.key == pygame.K_LEFT:
-					#Move the ship to the left.
-					self.ship.moving_left = True
-			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
-					#stops the ship from moving to the right.
-					self.ship.moving_right = False
-				if event.key == pygame.K_LEFT:
-					#stops the ship from moving to the left.
-					self.ship.moving_left = False
+				self._check_close_game(event)
+				self._check_keydown_events(event)
+				self._check_keyup_events(event)
+	
 
 
 	def _update_screen(self):
@@ -53,6 +41,30 @@ class AlienInvasion:
 		#Make the most recently drawn screen visible.
 		pygame.display.flip()
 
+	def _check_close_game(self, event):
+		"""Respond to exit game."""
+		if event.type == pygame.QUIT:
+				sys.exit()
+
+	def _check_keydown_events(self, event):
+		"""Respond to key press."""
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				#Move the ship to the right.
+				self.ship.moving_right = True
+			if event.key == pygame.K_LEFT:
+				#Move the ship to the left.
+				self.ship.moving_left = True
+
+	def _check_keyup_events(self, event):
+		"""Respond to key release."""
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_RIGHT:
+				#stops the ship from moving to the right.
+				self.ship.moving_right = False
+			if event.key == pygame.K_LEFT:
+				#stops the ship from moving to the left.
+				self.ship.moving_left = False
 
 if __name__ == "__main__":
 	#Make a game instance and run the game.
