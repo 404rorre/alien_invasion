3a51ad2406ddcf99dc4e32384705f80f03e34a0b
404rorre <404rorre.edoc@gmail.com>
2022-05-20 16:01:48 +0200

    Refactored Code for fleet creation.

--- alien_invasion.py
+++ alien_invasion.py
@@ -109,19 +109,34 @@ class AlienInvasion:
 
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
+		#Max Nr. Aliens in a row
+		number_aliens_x = self._get_number_aliens_x()
+		#Create first row of aliens.
+		for alien_number in range (number_aliens_x):
+			self._create_alien(alien_number)
+
+	def _create_alien(self, alien_number):
+		"""Simple method to add more aliens."""
+		alien = Alien(self)
+		alien_width = alien.rect.width
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		self.aliens.add(alien)
+
+	def _get_number_aliens_x(self):
+		"""Simple method to return max horizontal Nr.'s of aliens."""
 		#Create an alien and find the number of aliens in a row.
 		alien = Alien(self)
 		alien_width = alien.rect.width
 		available_space_x = self.settings.screen_width - (2 * alien_width)
 		number_aliens_x = available_space_x // (2 * alien_width)
-		#Create first row of aliens.
-		for alien_number in range (number_aliens_x):
-			#Create an alien and place it in the row
-			alien = Alien(self)
-			alien.x = alien_width + 2 * alien_width * alien_number
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+		return number_aliens_x
 
+
+
+	
+
+			
 
 
 
