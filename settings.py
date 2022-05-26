eece65b3ab9a7a6a8d97646bc7bbb781ef073694
404rorre <404rorre.edoc@gmail.com>
2022-05-26 13:48:45 +0200

    Increased bullet speed to 1.5

--- settings.py
+++ settings.py
@@ -10,11 +10,11 @@ class Settings:
 		#Ship settings
 		self.ship_speed = 1.5
 		#Bullet settings
-		self.bullet_speed = 1.0
-		self.bullet_width = 3
+		self.bullet_speed = 1.5
+		self.bullet_width = 4000
 		self.bullet_height = 15
 		self.bullet_color = (31, 81, 255)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 10
 		#Alien settings
 		self.alien_speed = 0.5
 		self.fleet_drop_speed = 10
